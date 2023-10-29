8de60481243dc8c142cfbf4a124cc9b9e815ff2f
Lucas Pereira <88951189+Luc45-Pereira@users.noreply.github.com>
2023-10-28 23:13:07 -0300

    fix deploy

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 """" Arquivo principal da API """
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from internal.routers import user, endereco, entrada, saida
 import dotenv
 
 dotenv.load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router, prefix="/user", tags=["user"])
 app.include_router(endereco.router, prefix="/endereco", tags=["endereco"])
